3b63f548020a1f2b64f40d215b51fab678b7cdca
Julien Smadja <julien.smadja@gmail.com>
2013-07-31 20:54:22 +0200

    Add Postgresql dependency

--- project/Build.scala
+++ project/Build.scala
@@ -10,11 +10,12 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
     javaCore,
     javaJdbc,
-    javaEbean
+    javaEbean,
+    "postgresql" % "postgresql" % "9.1-901.jdbc4"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+
   )
 
 }
