50712892f5808fc8b6b595c6a2f90d26a0860292
Julien Smadja <julien.smadja@gmail.com>
2013-07-31 21:18:55 +0200

    Add a candidate in database

--- app/Global.java
+++ app/Global.java
@@ -10,14 +10,22 @@ import models.*;
 public class Global extends GlobalSettings {
 
     public void onStart(Application app) {
+        //InitialData.insertFromFile(app);
         InitialData.insert(app);
+
     }
 
     static class InitialData {
-        public static void insert(Application app) {
+        public static void insertFromFile(Application app) {
             if(Ebean.find(Candidate.class).findRowCount() == 0) {
                 Map<String,List<Object>> all = (Map<String,List<Object>>)Yaml.load("initial-data.yml");
                 Ebean.save(all.get("candidates"));
+            }
+        }
+
+        public static void insert(Application app) {
+            if(Ebean.find(Candidate.class).findRowCount() == 0) {
+                Ebean.save(new Candidate("Julien", "Smadja", "julien.smadja@gmail.com"));
             }
         }
     }
