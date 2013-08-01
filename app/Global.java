4a808e7cf402539afdc6a3c32f96ff3013543c30
Julien Smadja <julien.smadja@gmail.com>
2013-08-01 20:56:56 +0200

    Add mail feature

--- app/Global.java
+++ app/Global.java
@@ -10,19 +10,10 @@ import models.*;
 public class Global extends GlobalSettings {
 
     public void onStart(Application app) {
-        //InitialData.insertFromFile(app);
         InitialData.insert(app);
-
     }
 
     static class InitialData {
-        public static void insertFromFile(Application app) {
-            if(Ebean.find(Candidate.class).findRowCount() == 0) {
-                Map<String,List<Object>> all = (Map<String,List<Object>>)Yaml.load("initial-data.yml");
-                Ebean.save(all.get("candidates"));
-            }
-        }
-
         public static void insert(Application app) {
             if(Ebean.find(Candidate.class).findRowCount() == 0) {
                 Ebean.save(new Candidate("Julien", "Smadja", "julien.smadja@gmail.com"));
--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,40 @@
 package controllers;
 
 import models.Candidate;
+import play.data.Form;
+import play.data.validation.Constraints;
 import play.mvc.Controller;
 import play.mvc.Result;
+import services.SimpleMail;
+
+import static play.data.Form.form;
 
 public class Application extends Controller {
 
-    public static Result candidates() {
-        return ok(views.html.index.render(Candidate.findAll()));
+    public static class Email {
+        @Constraints.Required
+        public String address;
+    }
+
+    public static Result sendEmail() {
+        Form<Email> form = form(Email.class).bindFromRequest();
+        if(form.hasErrors()) {
+            return badRequest(views.html.index.render(form, Candidate.findAll()));
+        } else {
+            Email data = form.get();
+            String email = data.address;
+            sendEmailTo(email);
+            return ok(views.html.index.render(form, Candidate.findAll()));
+        }
+    }
+
+    private static void sendEmailTo(String email) {
+        System.err.println("J'envoie un mail à "+email);
+        try {
+            new SimpleMail().send(email);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
