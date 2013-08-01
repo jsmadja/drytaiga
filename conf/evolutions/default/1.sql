9ba25aa5ab01a5877b8d25e959c14c55c32f0f38
Julien Smadja <julien.smadja@gmail.com>
2013-08-01 22:10:02 +0200

    Change db configuration

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -19,18 +19,9 @@ create table s3file (
 
 create sequence candidate_seq;
 
-
-
-
 # --- !Downs
 
-#SET REFERENTIAL_INTEGRITY FALSE;
-
 drop table if exists candidate;
-
 drop table if exists s3file;
-
-#SET REFERENTIAL_INTEGRITY TRUE;
-
 drop sequence if exists candidate_seq;
 
