2f2b2cd29747f90c143c317af456b50b33cdbc86
Julien Smadja <julien.smadja@gmail.com>
2013-08-08 21:49:47 +0200

    Change new relic version

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object ApplicationBuild extends Build {
     "postgresql" % "postgresql" % "9.1-901.jdbc4",
     "javax.mail" % "mail" % "1.4.1",
     "com.amazonaws" % "aws-java-sdk" % "1.3.11",
-    "com.newrelic.agent.java" % "newrelic-agent" % "2.x.x"
+    "com.newrelic.agent.java" % "newrelic-agent" % "2.21.0"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
