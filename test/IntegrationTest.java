cc4aaa30d23ac6d1e38a024e981cf11faf707785
Julien Smadja <julien.smadja@gmail.com>
2013-08-14 14:36:03 +0200

    Integration tests

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,3 +1,4 @@
+import org.junit.Ignore;
 import org.junit.Test;
 import play.libs.F.Callback;
 import play.test.TestBrowser;
@@ -7,16 +8,28 @@ import static play.test.Helpers.*;
 
 public class IntegrationTest {
 
-    /**
-     * add your integration test here
-     * in this example we just check if the welcome page is being shown
-     */
     @Test
-    public void test() {
+    public void should_display_login_form() {
         running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
                 browser.goTo("http://localhost:3333");
-                assertThat(browser.pageSource()).contains("Your new application is ready.");
+                assertThat(browser.pageSource()).contains("Login");
+            }
+        });
+    }
+
+    @Ignore
+    @Test
+    public void should_log_user() {
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+            public void invoke(TestBrowser browser) {
+                browser.goTo("http://localhost:3333");
+                browser.fill("#email").with("jdoe@mycompany.com");
+                browser.fill("#password").with("password");
+                System.err.println("before submit");
+                browser.click("#submit");
+                System.err.println("after submit");
+                assertThat(browser.pageSource()).contains("Dashboard");
             }
         });
     }
