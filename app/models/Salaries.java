c4b4ef515ce5b0483b25fc5fc941dc7016c83744
Julien Smadja <julien.smadja@gmail.com>
2013-08-14 19:14:02 +0200

    cleaning

--- app/models/Salaries.java
+++ app/models/Salaries.java
@@ -1,14 +1,11 @@
 package models;
 
-import play.Logger;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class Salaries {
 
     public static Map<String, String> list() {
-        Logger.debug("pouet");
         return new HashMap<String, String>() {{
             put("0", "30K - 40K");
             put("1", "41K - 50K");
