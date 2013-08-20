cd81f9d2219f2255634a8a01919c0325c70772e7
Julien Smadja <julien.smadja@gmail.com>
2013-08-20 17:27:05 +0200

    Refactoring

--- app/misc/TableFilter.java
+++ app/misc/TableFilter.java
@@ -3,8 +3,6 @@ package misc;
 import com.avaje.ebean.Page;
 import com.avaje.ebean.Query;
 import com.google.common.base.Predicate;
-import com.google.common.collect.Collections2;
-import org.apache.commons.lang3.StringUtils;
 import org.codehaus.jackson.node.ArrayNode;
 import org.codehaus.jackson.node.ObjectNode;
 import play.api.mvc.Call;
@@ -13,24 +11,20 @@ import play.mvc.Http;
 
 import javax.annotation.Nullable;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 
 import static com.google.common.collect.Collections2.filter;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
-public class TableFilter<T> {
+public class TableFilter {
 
     public static <T> ObjectNode createNode(Http.Request request, Query<T> query, final ColumnValueResolver... columnValuesResolvers) {
         Map<String, String[]> params = request.queryString();
         Collection<T> records = getRecords(query, params, columnValuesResolvers);
-
-        Integer totalDisplayRecords = records.size();
-
         ObjectNode result = Json.newObject();
         result.put("sEcho", Integer.valueOf(params.get("sEcho")[0]));
         result.put("iTotalRecords", (Integer) query.findRowCount());
-        result.put("iTotalDisplayRecords", totalDisplayRecords);
-
+        result.put("iTotalDisplayRecords", (Integer) records.size());
         ArrayNode array = result.putArray("aaData");
         for (T rowValue : records) {
             ObjectNode row = Json.newObject();
@@ -42,35 +36,34 @@ public class TableFilter<T> {
         return result;
     }
 
-    private static <T> Collection<T> getRecords(Query<T> query, Map<String, String[]> params, final ColumnValueResolver[] columnValuesResolvers) {
-        final String filter = params.get("sSearch")[0];
+    private static <T> Collection<T> getRecords(Query<T> query, Map<String, String[]> params, ColumnValueResolver[] columnValuesResolvers) {
+        String filter = params.get("sSearch")[0];
         Integer pageSize = Integer.valueOf(params.get("iDisplayLength")[0]);
         Integer startPage = Integer.valueOf(params.get("iDisplayStart")[0]) / pageSize;
-
         Page<T> page = query.
                 findPagingList(pageSize).
                 setFetchAhead(false).
                 getPage(startPage);
 
         Collection<T> records = page.getList();
-        if(StringUtils.isNotBlank(filter)) {
-            records = filter(records, new Predicate<T>() {
-                @Override
-                public boolean apply(@Nullable T t) {
-                    return isValid(filter, t, columnValuesResolvers);
-                }
-            });
+        if (isNotBlank(filter)) {
+            records = filter(records, withPredicate(columnValuesResolvers, filter));
         }
         return records;
     }
 
-    private static <T> boolean isValid(String filter, T rowValue, ColumnValueResolver[] columnValuesResolvers) {
-        for (ColumnValueResolver columnValuesResolver : columnValuesResolvers) {
-            if(columnValuesResolver.resolve(rowValue).contains(filter)) {
-                return true;
+    private static <T> Predicate<T> withPredicate(final ColumnValueResolver[] columnValuesResolvers, final String filter) {
+        return new Predicate<T>() {
+            @Override
+            public boolean apply(@Nullable T filtrable) {
+                for (ColumnValueResolver columnValuesResolver : columnValuesResolvers) {
+                    if (columnValuesResolver.resolve(filtrable).contains(filter)) {
+                        return true;
+                    }
+                }
+                return false;
             }
-        }
-        return false;
+        };
     }
 
     public static String url(String url, String value) {
